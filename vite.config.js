b351704dc62176d0354830ce164285380ec9cb06
Krishna Soni <krishnasoni.8596@gmail.com>
2026-01-16 02:09:37 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   base: "/React--portfolio/",
+   base: "/React--Portfolio/",
   plugins: [
     react(),
     tailwindcss(),
